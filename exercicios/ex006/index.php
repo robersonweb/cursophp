59872d0c43fbfb1a238bd789f64e220ce4b100c8
Roberson Ferreira Cardoso <120033226+robersonweb@users.noreply.github.com>
2023-05-09 10:34:59 -0300

    Update index.php

--- exercicios/ex006/index.php
+++ exercicios/ex006/index.php
@@ -8,26 +8,38 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-   <?php
-    // Capturando os dados do formulário retroalimentado
+        
 
-    $valor1 = $_GET['v1'] ?? 0;
-    $valor2 = $_GET['v2'] ?? 0;
+    <?php
+        // Capturando os dados do formulário retroalimentado
 
-     
-   ?>
+        // $valor1 = $_REQUEST['v1'] ?? 0;
+        // $valor2 = $_REQUEST['v2'] ?? 0;
+        $valor1 = 50;
+        $valor2 = 20;
+        
+    ?>
     <main>
-    <form action="<?php echo $_SERVER ['PHP_SELF'] ?>" method="get">
-        <label for="v1">Valor 1</label>
-        <input type="number" name="n1" id="n1" value="<?=$valor1?>">
-        <label for="v2">Valor 2</label>
-        <input type="number" name="n2" id="n2" value="<?=$valor2?>">
-        <input type="submit" value="Calcular" >
-    </form>
+        <h1>Somador de Valores</h1>
+        <form action="<?php echo $_SERVER ['PHP_SELF'] ?>" method="POST">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="n1" id="n1" value="<?=$valor1?>">
+            <label for="v2">Valor 2</label>
+            <input type="number" name="n2" id="n2" value="<?=$valor2?>">
+            <input type="submit" value="Calcular" >
+        </form>
+    </main>
+    <section>
+        <h2>Resultado do Cálculo</h2>
+        <?php
+            $soma = $valor1 + $valor2;
+            echo "<p>A soma é igual a $soma</p>";
+        ?>
+
+    </section>
 
 
-
-    </main>    
+        
 
  
 </body>
