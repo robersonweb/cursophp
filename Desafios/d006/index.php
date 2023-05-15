77256a7d57e7c625829ea1abd74b91cfacf781fa
Roberson Ferreira Cardoso <120033226+robersonweb@users.noreply.github.com>
2023-05-15 12:20:17 -0300

    desafio 006

--- Desafios/d006/index.php
+++ Desafios/d006/index.php
@@ -10,23 +10,44 @@
 <body>
     <?php
         $dividendo = $_GET['d1'] ?? 0;
-        $divisor = $GET['d2'] ?? 1;
+        $divisor = $_GET['d2'] ?? 1;
     ?>
     <main>
         <h1>Anatomia de uma Divisão</h1>
-        <form action="" method="get"></form>
-        <label for="d1">Dividendo</label>
-        <input type="number" name="d1" id="d1" value="<?=$dividendo?>">
-        <label for="d2">Divisor</label>
-        <input type="number" name="d2" id="d2" value="<?=$divisor?>">
-        <input type="submit" value="Calcular">
-
+        <form action="" method="get">
+            <label for="d1">Dividendo</label>
+            <input type="number" name="d1" id="d1" min="0" value="<?=$dividendo?>">
+            <label for="d2">Divisor</label>
+            <input type="number" name="d2" id="d2" min="1" value="<?=$divisor?>">
+            
+            <input type="submit" value="Calcular">
+        </form>
     </main>
     <section>
         <h2>Estrutura da Divisão</h2>
         <?php
-        
+            //cálculos
+            //$quociente = (int) ($dividendo / $divisor);
+            $quociente = intdiv($dividendo, $divisor);
+            $resto = $dividendo % $divisor;
+            // echo "<ul>";
+            // echo "<li>Dividendo: $dividendo</li>";
+            // echo "<li>Divisor: $divisor</li>";
+            // echo "<li>Quociente: $quociente</li>";
+            // echo "<li>Resto: $resto</li>";
+            // echo "</ul>";
         ?>
+
+        <table class="divisao">
+            <tr>
+                <td><?=$dividendo?></td>
+                <td><?=$divisor?></td>
+            </tr>
+            <tr>
+                <td><?=$resto?></td>
+                <td><?=$quociente?></td>
+            </tr>
+        </table>
     </section>
 </body>
 </html>
